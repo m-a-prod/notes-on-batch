7e59296da1df485d86378a3358d3495f83badba4
Максим Адаев <maximkad2008@gmail.com>
2022-01-05 18:27:30 +0300

    Created new file

---
+++ notes.bat
@@ -0,0 +1,7 @@
+@echo off
+:start
+cls
+echo Здраствуйте! Спасибо, что скачали мою программу, я вам очень благодарен!
+echo Приятного пользования! Чтобы сообщить мне об ошибке, я создал репозиторий
+echo на GitHub. https://github.com/maxim-adaev-production/notes
+pause>nul
