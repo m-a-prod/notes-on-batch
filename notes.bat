a3b25c84127e47a3205847bbb2c35e5b987140e5
Максим Адаев <maximkad2008@gmail.com>
2022-01-05 18:52:12 +0300

    Added some code

--- notes.bat
+++ notes.bat
@@ -1,7 +1,40 @@
 @echo off
+:init
+title notes on batch
+
 :start
 cls
-echo Здраствуйте! Спасибо, что скачали мою программу, я вам очень благодарен!
-echo Приятного пользования! Чтобы сообщить мне об ошибке, я создал репозиторий
-echo на GitHub. https://github.com/maxim-adaev-production/notes
-pause>nul
+echo Hello! Thank you for downloading my program, I am very grateful to you!
+echo Enjoy your use! To let me know about the error, I created a repository
+echo on GitHub. https://github.com/maxim-adaev-production/notes
+pause
+goto selectstorage
+
+:selectstorage
+cls
+echo Select the storage method.
+echo.
+echo 1. Locally (faster, but only on this computer)
+echo 2. On a WebDAV disk (slower, but you'll be able to use your notes
+echo on other computers)
+choice /c 12 /n
+if %errorlevel% == 1 goto localset
+if %errorlevel% == 2 goto webdavset
+
+:localset
+cls
+echo We haven't implemented this feature yet.
+echo Sorry :(
+echo.
+echo 0. Back
+choice /c 0 /n
+if %errorlevel% == 1 goto selectstorage
+
+:webdavset
+cls
+echo We haven't implemented this feature yet.
+echo Sorry :(
+echo.
+echo 0. Back
+choice /c 0 /n
+if %errorlevel% == 1 goto selectstorage 
